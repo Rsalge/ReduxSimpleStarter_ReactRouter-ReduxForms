30011d71859a9c41111f1cf17cf4cb094fbebcc6
Ross Salge <rossnsalge@gmail.com>
2018-04-27 17:30:24 -0500

    End of 126: Sets up async call to posts API in PostsIndex inside of componentDidMount

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
-
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 class PostsIndex extends Component {
+  componentDidMount() {
+    //this lifescycle method is automatically ran by react immediately after the component is added to the dom
+    this.props.fetchPosts();
+  }
+
   render() {
     return <div>Posts Index</div>;
   }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex); //doing this instead of mapDispatchToProps. fetchPosts is an action creator
