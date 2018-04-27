076dc2fbe40c9ddf2c67aada8a6f55593e3d463c
Ross Salge <rossnsalge@gmail.com>
2018-04-27 17:41:50 -0500

    End of 128: sets up rendering a  list of posts inside PostsIndex

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
@@ -6,10 +7,27 @@ class PostsIndex extends Component {
     //this lifescycle method is automatically ran by react immediately after the component is added to the dom
     this.props.fetchPosts();
   }
-
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
   render() {
-    return <div>Posts Index</div>;
+    return (
+      <div>
+        <h3> Posts </h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
   }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex); //doing this instead of mapDispatchToProps. fetchPosts is an action creator
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex); //doing this instead of mapDispatchToProps. fetchPosts is an action creator
