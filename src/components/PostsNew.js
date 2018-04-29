d5de8d0925aa361e26663e6f219f1998c19614c0
Ross Salge <rossnsalge@gmail.com>
2018-04-29 10:11:34 -0500

    End of 136: Sets up validate function for verifying valid Fields in PostsNew

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -22,7 +22,11 @@ class PostsNew extends Component {
           label="Title For Post"
           component={this.renderField}
         />
-        <Field name="tags" label="Tags" component={this.renderField} />
+        <Field
+          name="categories"
+          label="Categories"
+          component={this.renderField}
+        />
         <Field
           name="content"
           label="Post Content"
@@ -33,7 +37,18 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  //values is an objuect whose keys are field titles and values are the content for those fields
+  //whenever the user attempts to submit the form this function will be called
+  const errors = {};
+  //Validate the inputs from 'values'
+
+  return errors;
+  //if errors is an empty oject then the form is fine to submit, otherwise redux-form assumes form is invalid
+}
+
 export default reduxForm({
+  validate,
   form: "PostsNewForm"
   /* form key value 'PostsNewForm' is the name of the form, useful when wanting to show several different forms
   such as signup/sign in */
