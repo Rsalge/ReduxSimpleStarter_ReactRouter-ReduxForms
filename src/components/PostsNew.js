ce56e14d14153bc887a799dc38222e8bf92ff5c8
Ross Salge <rossnsalge@gmail.com>
2018-04-29 09:37:57 -0500

    End of 133: Begins setup of reduxForm in PostsNew

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
-
+import { Field, reduxForm } from "redux-form";
+/*reduxForm is very similar to the connect function from redux, it is what allows our 
+ component to talk directly to the redux store
+*/
 class PostsNew extends Component {
   render() {
-    return <div>Posts New!</div>;
+    return <form>
+      <Field
+      name='title'
+      componenet={}
+      />
+    </form>;
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+  /* form key value 'PostsNewForm' is the name of the form, useful when wanting to show several different forms
+  such as signup/sign in */
+})(PostsNew);
