84d174e53348c6f3c088f6d2dcf9c6023fa09b06
Ross Salge <rossnsalge@gmail.com>
2018-04-29 09:49:16 -0500

    End of 134: Sets up title field for reduxForm in PostsNew

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -4,18 +4,25 @@ import { Field, reduxForm } from "redux-form";
  component to talk directly to the redux store
 */
 class PostsNew extends Component {
+  renderTitleField(field) {
+    // ...field.input is an object with onChange and other event handlers that exist in it, passing all of these to <input>
+    return (
+      <div>
+        <input type="text" {...field.input} />
+      </div>
+    );
+  }
   render() {
-    return <form>
-      <Field
-      name='title'
-      componenet={}
-      />
-    </form>;
+    return (
+      <form>
+        <Field name="title" componenet={this.renderTitleField} />
+      </form>
+    );
   }
 }
 
 export default reduxForm({
-  form: 'PostsNewForm'
+  form: "PostsNewForm"
   /* form key value 'PostsNewForm' is the name of the form, useful when wanting to show several different forms
   such as signup/sign in */
 })(PostsNew);
