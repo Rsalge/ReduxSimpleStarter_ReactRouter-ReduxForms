61eed11293f49c3279264443fa7e78ae12eaf38d
Ross Salge <rossnsalge@gmail.com>
2018-04-29 08:42:57 -0500

    End of 132: Imports redux-form "formReducer" method into reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
 import PostsReducer from "./reducer_posts";
+import { reducer as formReducer } from "redux-form"; //the as keyword renames the "reducer" method from redux-form to "formReducer"
+
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
